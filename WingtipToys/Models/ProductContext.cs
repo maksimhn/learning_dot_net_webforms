f0f650fb664af973ff4531e4ad1ebb9785850297
maksimhn <maksimhranin@gmail.com>
2015-12-16 11:01:30 -0500

    Add shopping cart and page and logic

--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -11,5 +11,6 @@ namespace WingtipToys.Models
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
     }
 }
